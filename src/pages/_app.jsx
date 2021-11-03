93e64c5dc757f202f4aefdc8e04f678f71508489
Nathan Yang <nathan.yang16@gmail.com>
2021-11-03 13:54:49 -0400

    added styling to components

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { GlobalProvider } from '../context/Provider';
+import styles from '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-      <GlobalProvider>
-          <Component {...pageProps} />
-      </GlobalProvider>
-  );
+	return (
+		<GlobalProvider>
+			<Component {...pageProps} />
+		</GlobalProvider>
+	);
 }
 
 export default MyApp;
