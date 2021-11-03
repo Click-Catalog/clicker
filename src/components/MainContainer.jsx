14b35965adb01867d5ff263f70f66c1aa616ed8d
Jason <zeng.jason94@gmail.com>
2021-11-03 18:07:33 -0400

    fixing conflict

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,21 +6,6 @@ import styles from '../styles/MainContainer.module.css';
 
 // import { useStore } from '../context/Provider';
 
-<<<<<<< HEAD
-export default function MainContainer() {
-	// const { mustWatchContext, interestedContext, watchedContext } = useStore();
-	// const { mustWatch, mustWatchDispatch } = mustWatchContext;
-	// const { interested, interestedDispatch } = interestedContext;
-	// const { watched, watchedDispatch } = watchedContext;
-	// console.log('mustWatchContext: ' + mustWatchContext);
-	return (
-		<div className={styles.container}>
-			{/* < Login /> */}
-			<MovieContainer />
-		</div>
-	);
-}
-=======
 export default function MainContainer( ) {
 
   // const [user, logIn] = useState('');
@@ -38,4 +23,3 @@ export default function MainContainer( ) {
     </div>
   )
 }
->>>>>>> ae6e85b33d343f358e7a43f8a6ae6246aee3f3ea
