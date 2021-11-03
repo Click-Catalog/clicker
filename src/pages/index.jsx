3f8eaf115ee61f5dc0fd8750149a3667313027d1
brianvazquez97 <bjmv97@gmail.com>
2021-11-03 17:36:12 -0400

    log in sign up moved components and added some css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import MainContainer from "../components/MainContainer"
+import Login from '../components/Login'
 
 export default function index(props) {
   return (
     <div>
+      < Login />
       < MainContainer />
     </div>
   )
